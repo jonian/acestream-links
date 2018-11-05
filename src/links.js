44d40d78f2080cf24cee6321ca4f916e536f7888
Jonian Guveli <jonian@hardpixel.eu>
2018-11-05 19:24:23 +0200

    rewrite extension

--- src/links.js
+++ src/links.js
@@ -1,14 +1,40 @@
-let xpath = "//*[not(self::a)][contains(text(), 'acestream://')]";
-let links = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+function addTextNode(node, string) {
+  if (!string.length) return;
 
-for (let index = 0; index < links.snapshotLength; index++) {
-  let node = links.snapshotItem(index);
-  let href = node.textContent.match(/acestream\:\/\/[\w\d]+/);
+  let text = document.createTextNode(string);
+  node.appendChild(text);
+}
+
+function addLinkNode(node, href) {
+  if (!href.length) return;
+
   let link = document.createElement('a');
+  node.appendChild(link);
 
   link.href = href;
-  link.text = node.textContent;
+  link.text = href;
+}
 
-  node.firstChild.remove();
-  node.appendChild(link);
+let linkRegex = new RegExp(/acestream\:\/\/[\w\d]+/);
+let lineRegex = new RegExp(`\\n?.*${linkRegex.source}.*\\n?`, 'g');
+
+let linkXpath = "//*[not(self::a)][contains(text(), 'acestream://')]";
+let linkItems = document.evaluate(linkXpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+
+for (let index = 0; index < linkItems.snapshotLength; index++) {
+  let node = linkItems.snapshotItem(index);
+  let data = node.textContent.match(lineRegex);
+
+  if (data) {
+    node.firstChild.remove();
+
+    data.forEach(function (item) {
+      let text = item.split(linkRegex);
+      let href = item.match(linkRegex);
+
+      addTextNode(node, text[0]);
+      addLinkNode(node, href)
+      addTextNode(node, text[1]);
+    });
+  }
 }
