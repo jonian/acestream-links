d93f3fae0b62121c827a8002b4d16e8237cc9591
Jonian Guveli <jonian@hardpixel.eu>
2018-11-05 23:43:32 +0200

    wrap link generation into function

--- src/links.js
+++ src/links.js
@@ -25,26 +25,30 @@ function addLinkNode(node, href) {
   node.appendChild(link);
 }
 
-let linkRegex = new RegExp(/acestream\:\/\/[\w\d]+/);
-let lineRegex = new RegExp(`\\n?.*${linkRegex.source}.*\\n?`, 'g');
+function generateLinks(content) {
+  let linkRegex = new RegExp(/acestream\:\/\/[\w\d]+/);
+  let lineRegex = new RegExp(`\\n?.*${linkRegex.source}.*\\n?`, 'g');
 
-let linkXpath = "//*[not(self::a)][contains(text(), 'acestream://')]";
-let linkItems = document.evaluate(linkXpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+  let linkXpath = "//*[not(self::a)][contains(text(), 'acestream://')]";
+  let linkItems = document.evaluate(linkXpath, content, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-for (let index = 0; index < linkItems.snapshotLength; index++) {
-  let node = linkItems.snapshotItem(index);
-  let data = node.textContent.match(lineRegex);
+  for (let index = 0; index < linkItems.snapshotLength; index++) {
+    let node = linkItems.snapshotItem(index);
+    let data = node.textContent.match(lineRegex);
 
-  if (data) {
-    node.firstChild.remove();
+    if (data) {
+      node.firstChild.remove();
 
-    data.forEach(function (item) {
-      let text = item.split(linkRegex);
-      let href = item.match(linkRegex);
+      data.forEach(function (item) {
+        let text = item.split(linkRegex);
+        let href = item.match(linkRegex);
 
-      addTextNode(node, text[0]);
-      addLinkNode(node, href)
-      addTextNode(node, text[1]);
-    });
+        addTextNode(node, text[0]);
+        addLinkNode(node, href[0]);
+        addTextNode(node, text[1]);
+      });
+    }
   }
 }
+
+generateLinks(document.body);
