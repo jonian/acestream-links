8b76be903553f9cffb84331ad9472c9522d13328
Jonian Guveli <jonian@hardpixel.eu>
2018-11-05 16:11:53 +0200

    use xpath instead of regex to replace links

--- src/links.js
+++ src/links.js
@@ -1,6 +1,14 @@
-var aceRegex = /(?!<a.+)acestream\:\/\/[\w\d]+(?!.+a>)/g;
-var aceLinks = function (match) {
-  return '<a href="' + match + '">' + match + '</a>';
-};
+let xpath = "//*[not(self::a)][contains(text(), 'acestream://')]";
+let links = document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
 
-document.body.innerHTML = document.body.innerHTML.replace(aceRegex, aceLinks);
+for (let index = 0; index < links.snapshotLength; index++) {
+  let node = links.snapshotItem(index);
+  let href = node.textContent.match(/acestream\:\/\/[\w\d]+/);
+  let link = document.createElement('a');
+
+  link.href = href;
+  link.text = node.textContent;
+
+  node.firstChild.remove();
+  node.appendChild(link);
+}
